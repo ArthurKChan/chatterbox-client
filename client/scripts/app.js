02a2d078e394bbcde75c1b243484da414e83c82e
Arfurboy <arfurboy@gmail.com>
2014-11-10 16:31:05 -0800

    Retrieve and Send messages, stops scripts in messages from running

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,48 @@
 // YOUR CODE HERE:
+var App = function(){
+  this.server = 'https://api.parse.com/1/classes/chatterbox';
 
+}
+
+App.prototype.init = function(){
+
+}
+
+App.prototype.send = function(message){
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data){
+      console.log('send success:',data);
+    },
+    error: function(data){
+      console.log('send error:',data);
+    },
+  });
+}
+
+App.prototype.fetch = function(){
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    success: refreshMsgLog,
+    error: function(data){
+      console.log('error');
+    }
+  });
+}
+
+window.refreshMsgLog = function(data){
+  _.each(data.results, function(msg, index, collection){
+    var username = JSON.stringify(msg.username);
+    username = username.split('').splice(1, username.length-2).join('');
+    var message = JSON.stringify(msg.text);
+    message = message.split('').splice(1, message.length-2).join('');
+    $('body').find('#chatLog').append('<div>'+ username + ': ' + message +'</div>')
+  });
+}
+
+var app = new App();
+app.fetch();
