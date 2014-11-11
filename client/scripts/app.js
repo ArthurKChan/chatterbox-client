e47ce03940eb96091cdb13e764d0174783bd77aa
Arfurboy <arfurboy@gmail.com>
2014-11-11 10:45:46 -0800

    pass all spec runner tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,10 @@ var App = function(){
 }
 
 App.prototype.init = function(){
-
+  this.fetch();
+  setInterval(this.fetch.bind(this), 5000);
+  $('body').find('#send .submit').on('submit',this.handleSubmit);
+  $('#main').find('.username').on('click', this.addFriend);
 }
 
 App.prototype.send = function(message){
@@ -15,7 +18,7 @@ App.prototype.send = function(message){
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function(data){
-      console.log('send success:',data);
+      console.log('send success');
     },
     error: function(data){
       console.log('send error:',data);
@@ -27,6 +30,7 @@ App.prototype.fetch = function(){
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
+    data: 'order=-createdAt; limit=15',
     success: refreshMsgLog,
     error: function(data){
       console.log('error');
@@ -34,19 +38,53 @@ App.prototype.fetch = function(){
   });
 }
 
+App.prototype.clearMessages = function(){
+  $('#chats').children().remove();
+}
+
+App.prototype.addMessage = function(message){
+  var username = antiXSS(message.username);
+  var message = antiXSS(message.text);
+  $('body').find('#chats').append('<div class="msg"> <span class="username">'
+    +username+'</span> <span class="msgContent">'+message+'</span> </div>');
+  // $('body').find('#chats').append('<div class="msg '+username+'">'+username+': '+message+'</div>');
+};
+
 window.refreshMsgLog = function(data){
-  $('body').find('#chatLog').find('.msg').remove();
+  $('body').find('#chats').find('.msg').remove();
   _.each(data.results, function(msg, index, collection){
-    var username = JSON.stringify(msg.username);
-    username = username.split('').splice(1, username.length-2).join('');
-    var message = JSON.stringify(msg.text);
-    message = message.split('').splice(1, message.length-2).join('');
-    $('body').find('#chatLog').append('<div class="msg">'+ username + ': ' + message +'</div>')
+    var username = antiXSS(msg.username);
+    var message = antiXSS(msg.text);
+    //var roomname = antiXSS(msg.roomname);
+    $('body').find('#chats').append('<div class="msg '+username+'">'+username+': '+message+'</div>');
   });
+}
+
+window.antiXSS = function(item){
+  if (item === undefined ){
+    return '';
+  }else{
+    item = item.replace(/\&/g,'&amp').replace(/\</g,'&lt')
+    .replace(/\>/,'&gt').replace(/\"/,'&quot').replace(/\'/,'&#x27')
+    .replace(/\//,'&#x2F')
+    return item;
+  }
+}
+
+App.prototype.addRoom = function(room){
+  $('#roomSelect').append('<div id="'+room+'"></div>')
+}
+
+App.prototype.addFriend = function(friend){
+  //TODO
+
+}
+
+App.prototype.handleSubmit = function(){
+
 }
 
 var app = new App();
 $('document').ready(function(){
-  app.fetch();
-  setInterval(app.fetch.bind(app), 5000);
+  app.init();
 });
