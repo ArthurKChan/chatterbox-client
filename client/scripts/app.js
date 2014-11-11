6be77ab06df5dc21375d7bae61b93d7ceffd8e11
Arfurboy <arfurboy@gmail.com>
2014-11-10 16:39:22 -0800

    Refresh chat log every 5 seconds

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -35,14 +35,18 @@ App.prototype.fetch = function(){
 }
 
 window.refreshMsgLog = function(data){
+  $('body').find('#chatLog').find('.msg').remove();
   _.each(data.results, function(msg, index, collection){
     var username = JSON.stringify(msg.username);
     username = username.split('').splice(1, username.length-2).join('');
     var message = JSON.stringify(msg.text);
     message = message.split('').splice(1, message.length-2).join('');
-    $('body').find('#chatLog').append('<div>'+ username + ': ' + message +'</div>')
+    $('body').find('#chatLog').append('<div class="msg">'+ username + ': ' + message +'</div>')
   });
 }
 
 var app = new App();
-app.fetch();
+$('document').ready(function(){
+  app.fetch();
+  setInterval(app.fetch.bind(app), 5000);
+});
